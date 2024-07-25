ab3e7169480a97341712774e6e8fafa952065fa1
Sipho <sipho.sithebe@gmail.com>
2024-07-25 13:37:37 +0200

    Created Method to Click White Paper & Ebook

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -15,14 +15,35 @@ public class LandingPage {
     @FindBy(xpath = "//span[contains(.,'Resources')]")
     WebElement Resource_xpath;
 
+    @FindBy(xpath = "(//span[contains(.,'White Papers & eBooks')])[3]")
+    WebElement whitePapersEBooks_xpath;
+
+    @FindBy(xpath = "//button[contains(.,'Accept All Cookies')]")
+    WebElement acceptAllCookies_xpath;
+
+
     public LandingPage(WebDriver driver) {
         this.driver = driver;
     }
 
     public void verifyResourceTileisDipslayed() {
-
         new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(Resource_xpath));
         String Resources = Resource_xpath.getText();
         Assert.assertEquals(Resources, "RESOURCES");
     }
+
+    public void clickWhitePaper() throws InterruptedException {
+
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(whitePapersEBooks_xpath));
+
+        whitePapersEBooks_xpath.click();
+    }
+
+    public void acceptAllCookies() throws InterruptedException {
+
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(acceptAllCookies_xpath));
+
+        acceptAllCookies_xpath.click();
+    }
+
 }
--- src/test/java/Tests/KurtosysTest.java
+++ src/test/java/Tests/KurtosysTest.java
@@ -5,10 +5,21 @@ import org.testng.annotations.Test;
 
 @Test
 public class KurtosysTest extends Base {
-    public void VerifyResources(){
+
+
+    public void VerifyResources() throws InterruptedException {
+        landingPage.acceptAllCookies();
         landingPage.verifyResourceTileisDipslayed();
-        takesScreenshots.takesSnapShot(driver,"KurtosysLandingPage");
+        takesScreenshots.takesSnapShot(driver, "KurtosysLandingPage");
     }
+
+    @Test(priority = 1)
+    public void ClickWhitePaper() throws InterruptedException {
+        landingPage.clickWhitePaper();
+
+        takesScreenshots.takesSnapShot(driver, "WhitePaperPage");
+    }
+
     @AfterTest
     public void closeBrowser() {
         driver.quit();
